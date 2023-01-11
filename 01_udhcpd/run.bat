7dbeef42edb97dfca922b1dda932dcb99bfc2030
uplusplus <yjcpu@163.com>
2023-01-11 10:57:09 +0800

    build for arm/arm64

--- 01_udhcpd/run.bat
+++ 01_udhcpd/run.bat
@@ -1,7 +1,7 @@
 adb remount
 set remote_dir=/data/udhcpd/
 adb shell mkdir %remote_dir%
-adb push busybox_UDHCPD %remote_dir%udhcpd
+adb push udhcpd_arm64 %remote_dir%udhcpd
 adb push init.udhcpd %remote_dir%
 adb push udhcpd.conf %remote_dir%
 
